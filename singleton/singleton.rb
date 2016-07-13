29d7befcf0755ca7d20734a6fa9f62da7641efdb
Hugo Benicio <hbobenicio@gmail.com>
2016-07-13 12:10:03 -0300

    Improving singleton example.

--- singleton/singleton.rb
+++ singleton/singleton.rb
@@ -2,18 +2,25 @@
 
 require 'singleton'
 
+# Declaring a singleton class
 class MySingleton
   include Singleton
   
   attr_accessor :info
+
+  def initialize
+    puts "Singleton constructor is called just once."
+  end
 end
 
+# Trying to instanciate it manually will raise an exception
 begin
   MySingleton.new
 rescue NoMethodError
   puts "Singleton classes have private constructors."
 end
 
+# Testing it
 MySingleton.instance.info = "Hello, Singleton"
 
 test = MySingleton.instance
