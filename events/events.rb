9f6d900f68a0dbf9ba8373c5635b40b34e21a888
Hugo Benício <hbobenicio@gmail.com>
2016-07-15 06:53:13 -0300

    Adding events example

---
+++ events/events.rb
@@ -0,0 +1,26 @@
+#!/usr/bin/env
+
+module EventHandler
+  attr_accessor :events_table
+  
+  def connect(event, &slot_block)
+    @events_table ||= {}
+    
+    if @events_table[event].nil?
+      @events_table[event] = [slot_block]
+    else
+      @events_table[event] << slot_block
+    end
+  end
+  
+  def emit(event)
+    @events_table[event].each do |slot_block|
+      slot_block.call
+    end
+  end
+end
+
+class Foo
+  include EventHandler
+end
+
